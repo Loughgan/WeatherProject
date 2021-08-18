3f80ae86b2af2096a4ea92217f397ea5e139faa1
Loughgan <loganlogan123@live.com>
2021-08-18 13:18:58 +1200

    Added a test arduino program to test the serial communication with python without using all the sensors. Also got serial communication working and formatted the data received.

---
+++ Arduino_Serial_Test/Arduino_Serial_Test.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("H:79.4");
+  Serial.println("P:986.40");
+  Serial.println("F:32.00");
+  Serial.println("C:0.00");
+  delay(10000);
+
+}
--- data.py
+++ data.py
@@ -1,7 +1,28 @@
 import serial
 
+#setting up the serial input reading
 ser = serial.Serial()
 ser.baudrate = 9600
-
+ser.port = 'COM6'
+ser.open()
+#reading each line as it should be in the order: Humidity>Pressure>Temp F>Temp C
+try:
+	h = str(ser.readline())
+	p = str(ser.readline())
+	f = str(ser.readline())
+	c = str(ser.readline())
+	#processing the strings to remove useless characters
+	h = h[h.index("H"):h.index("\\")]
+	p = p[p.index("P"):p.index("\\")]
+	f = f[f.index("F"):f.index("\\")]
+	c = c[c.index("C"):c.index("\\")]
+	#printing the data for viewing purposes
+	print(h)
+	print(p)
+	print(f)
+	print(c)
+except:
+	print("Oopsie! Logan goof'd!")
+ser.close()
 print(ser)
-print ("This is just a test for getting python working")
+print ("Serial Test Done")
